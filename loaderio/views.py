e6570f24f62849430e55187c1c3cd85464303c61
Lorin Hochstein <lorin.hochstein@sendgrid.com>
2014-05-01 15:41:28 -0400

    Only show token if enabled

--- loaderio/views.py
+++ loaderio/views.py
@@ -5,5 +5,6 @@ from loaderio.models import Validation
 
 def show_token(request, *args, **kwargs):
     validation = get_object_or_404(Validation,
-                                   token=kwargs.get("token"))
+                                   token=kwargs.get("token"),
+                                   enabled=True)
     return HttpResponse(validation.token, content_type="text/plain")
