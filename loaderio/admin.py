d0ffc861fd5202bb6cbed480f9f05c2c58ee0337
Lorin Hochstein <lorin.hochstein@sendgrid.com>
2014-05-01 15:51:36 -0400

    Enable/disable from list view

--- loaderio/admin.py
+++ loaderio/admin.py
@@ -4,5 +4,7 @@ from loaderio.models import Validation
 
 class ValidationAdmin(admin.ModelAdmin):
     fields = ['token', 'enabled']
+    list_display = ['token', 'enabled']
+    list_editable = ['enabled']
 
 admin.site.register(Validation, ValidationAdmin)
